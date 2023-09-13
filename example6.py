c4e9dbf7f21c7a2eef0c263fcb652d0c0f6a2e2c
sneha <rosh3267@gmail.com>
2023-09-13 15:26:35 +0530

    loop #2

--- example6.py
+++ example6.py
@@ -1,5 +1,5 @@
 from turtle import *
-speed = ('slowest')
+speed("fastest")
 dis=[50, 50, 50, 50, 50, 60, 60, 25, 60, 150, 50, 30, 50, 90, 76, 200, 80, 100, 90]
 ngl=[90, 60, 60, 60, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 90, 240, 300, 310, 200]
 for d, a in zip(dis, ngl):
@@ -8,6 +8,5 @@ for d, a in zip(dis, ngl):
       rt(abs(a))
     else:
       lt(a)
-
-#hideturtle()
+hideturtle()
 mainloop()
